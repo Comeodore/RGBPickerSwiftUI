7a6d139800386a21938256fb30c534b59269cccc
Maksymchuk Vladimir <maksymchuk.vladimir@gmail.com>
2025-05-08 01:05:02 +0300

    Initial Commit

---
+++ RGBPickerSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RGBPickerSwiftUI
+//
+//  Created by Vladimir Maksymchuk on 08.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RGBPickerSwiftUI/RGBPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBPickerSwiftUIApp.swift
+//  RGBPickerSwiftUI
+//
+//  Created by Vladimir Maksymchuk on 08.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct RGBPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
